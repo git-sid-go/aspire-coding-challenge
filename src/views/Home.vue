29ad42e5574c86ad6a47d1ede4cb1f037c7719aa
git-sid-go <siddhuvaradan@gmail.com>
2021-09-04 11:54:51 +0530

    feat: set up tailwind and component structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div id="nav">
+      <router-link to="/apply-and-manage-loans">Loans</router-link> |
+      <router-link to="/dashboard">Dashboard</router-link>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  name: "Home",
 });
 </script>
