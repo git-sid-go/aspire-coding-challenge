a93714cc3ab4fd557dffbf4f62a2e0966bd054c6
git-sid-go <siddhuvaradan@gmail.com>
2021-09-05 06:14:37 +0530

    feat: refactor expansion item component for reuse

--- src/components/RecentTransactions.vue
+++ src/components/RecentTransactions.vue
@@ -1,42 +1,31 @@
 <template>
-  <div class="border border-grey my-6 rounded-lg">
-    <q-expansion-item
-      expand-separator
-      expand-icon="img:/img/icons/down-arrow.svg"
-      header-class="bg-app-bluish-grey rounded-lg"
-    >
-      <template v-slot:header>
-        <div class="flex items-center text-app-primary font-semibold flex-1">
-          <img :src="transactionIcon" />
-          <div class="ml-3">Recent Transactions</div>
-        </div>
+  <BaseExpansionItem headerTitle="Recent Transactions">
+    <div class="bg-white px-6">
+      <template :key="item.name" v-for="item in recent_transactions_list">
+        <TransactionItem
+          :name="item.name"
+          :amount="item.amount"
+          :type="item.type"
+          :date="item.date"
+          :transactionType="item.transactionType"
+          :icon="item.icon"
+          :bgColor="item.color"
+        />
       </template>
-      <div class="bg-white px-6">
-        <template :key="item.name" v-for="item in recent_transactions_list">
-          <TransactionItem
-            :name="item.name"
-            :amount="item.amount"
-            :type="item.type"
-            :date="item.date"
-            :transactionType="item.transactionType"
-            :icon="item.icon"
-            :bgColor="item.color"
-          />
-        </template>
-      </div>
-    </q-expansion-item>
-  </div>
+    </div>
+  </BaseExpansionItem>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import TransactionItem from "@/components/TransactionItem.vue";
+import BaseExpansionItem from "@/legos/BaseExpansionItem.vue";
 import { CommonIcons } from "@/assets/assets";
 import { RECENT_TRANSACTIONS } from "@/composables/useRecentTransactions";
 
 export default defineComponent({
   name: "RecentTransactions",
-  components: { TransactionItem },
+  components: { TransactionItem, BaseExpansionItem },
   setup() {
     return {
       transactionIcon: CommonIcons.transaction,
---
+++ src/legos/BaseExpansionItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="border border-grey my-6 rounded-lg">
+    <q-expansion-item
+      expand-separator
+      expand-icon="img:/img/icons/down-arrow.svg"
+      header-class="bg-app-bluish-grey rounded-lg"
+    >
+      <template v-slot:header>
+        <div class="flex items-center text-app-primary font-semibold flex-1">
+          <img :src="transactionIcon" />
+          <div class="ml-3">{{ headerTitle }}</div>
+        </div>
+      </template>
+      <slot></slot>
+    </q-expansion-item>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BaseExpansionItem",
+  props: {
+    headerTitle: String,
+  },
+});
+</script>
